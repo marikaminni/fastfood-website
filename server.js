fe389ca9a4e3ee41ceaad022ac97177656754d0c
marikaminni <marika.minni@outlook.it>
2024-11-12 19:54:45 +0100

    eliminato require Product che faceva riferimento ad un file che ho eliminato

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-const { Product } = require("./models/Product.js");
 const express = require("express");
 const app = express();
 const path = require("path");
-console.log(Product);
+
 require("dotenv").config({ path: "./config/.env" });
 app.engine(".html", require("ejs").__express);
 
@@ -22,5 +21,3 @@ const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
   console.log(`Server avviato e in ascolto sulla porta ${PORT}`);
 });
-
-console.log(process.env.MAPS_API_KEY);
