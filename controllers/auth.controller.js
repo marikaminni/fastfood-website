914c9c61d9bc703da09d24a6cf81236f7e1a9d21
marikaminni <marika.minni@outlook.it>
2024-12-11 21:05:18 +0100

    Added logout

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -110,6 +110,18 @@ const refreshToken = (req, res) => {
   }
 };
 
-module.exports = { signUp, signIn, refreshToken };
+const logout = async (req, res) => {
+  try {
+    res.clearCookie("refreshToken", {
+      secure: true,
+      httpOnly: true,
+      sameSite: "strict",
+    });
+    res.status(200).json({ message: "Logout successfully" });
+  } catch (error) {
+    console.error("Error during logout", error);
+    res.status(500).json({ message: "Error during logout" });
+  }
+};
 
-//todo: add logout
+module.exports = { signUp, signIn, refreshToken, logout };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,10 +4,11 @@ const {
   signUp,
   signIn,
   refreshToken,
+  logout,
 } = require("../controllers/auth.controller");
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 router.get("/refresh-token", refreshToken);
-
+router.post("/logout", logout);
 module.exports = router;
